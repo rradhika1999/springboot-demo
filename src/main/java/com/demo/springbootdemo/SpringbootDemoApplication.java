a810cb3f1c65bc9a574d663e5d0076e61db3a355
rradhika1999 <39021888+rradhika1999@users.noreply.github.com>
2021-08-25 19:44:17 +0530

    Initial commit

---
+++ src/main/java/com/demo/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,24 @@
+package com.demo.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootDemoApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index()
+	{
+		return "Welcome to SpringBoot...";
+	}
+
+}
